b8546e4f0b733b1f4886eac1362b712a01a046f4
naina608 <104517150+naina608@users.noreply.github.com>
2023-04-22 11:06:22 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -1,7 +1,7 @@
 const jwtToken=require("jsonwebtoken");
 const sendCookie=(user,res,message,statuscode=200)=>{
 const token=jwtToken.sign({_id:user._id},process.env.SECRET_KEY);
-    return res.status(statuscode).cookie("token",token,{
+    res.status(statuscode).cookie("token",token,{
         httpOnly:true,
         maxAge:15*60*1000,
         sameSite:process.env.NODE_ENV=="development"?"lax":"none",
