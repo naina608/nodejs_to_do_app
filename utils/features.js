8e7255153603fe76dfd711387e121d5c482eadb0
naina608 <104517150+naina608@users.noreply.github.com>
2023-04-25 01:54:11 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -6,8 +6,8 @@ const token=jwtToken.sign({_id:user._id},process.env.SECRET_KEY);
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
       success: true,
